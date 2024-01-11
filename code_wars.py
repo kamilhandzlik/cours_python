3ccae4a420fd481839880e1d896f933aa793359a
Kamil Handzlik <kamol0209@gmail.com>
2024-01-11 19:41:41 +0100

    more of my daily practice

--- code_wars.py
+++ code_wars.py
@@ -302,3 +302,132 @@ def count(s):
 # Test case
 print(count('aba'))
 print(count(''))
+
+#####################################################################################
+###############   ZADANIE 9      ####################################################
+#####################################################################################
+
+# Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+# Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+# rozwiązanie 1
+def paperwork(n, m):
+    k = 0
+    if n < 0 or m < 0:
+        n, m = 0, 0
+    else:
+        k = n * m
+    return k
+
+#rozwiązanie 2
+def paperwork(n, m):
+    return max(n * m, 0) if n >= 0 and m >= 0 else 0
+
+# Testy
+print(paperwork(5, 10))  # Oczekiwane: 50
+print(paperwork(-5, 10))  # Oczekiwane: 0
+print(paperwork(5, -10))  # Oczekiwane: 0
+print(paperwork(-5, -10))  # Oczekiwane: 0
+
+#####################################################################################
+###############   ZADANIE 10      ####################################################
+#####################################################################################
+
+# Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+# Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+def binary_array_to_number(arr):
+    decimal_num = int("".join(map(str, arr)), 2)
+    return decimal_num
+
+# Przykłady użycia
+binary_array1 = [1, 0, 1, 0, 1]
+binary_array2 = [1, 1, 0, 0, 1]
+
+result1 = binary_array_to_number(binary_array1)
+result2 = binary_array_to_number(binary_array2)
+
+print(result1)  # Oczekiwane: 21
+print(result2)  # Oczekiwane: 25
+#####################################################################################
+###############   ZADANIE 11      ####################################################
+#####################################################################################
+
+# Introduction
+# The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+# Task
+# Given a year, return the century it is in
+
+def century(year):
+    # Calculate the century using integer division
+    century_num = (year - 1) // 100 + 1
+    return century_num
+
+# Examples
+print(century(1705))  # Expected output: 18
+print(century(1900))  # Expected output: 19
+print(century(2000))  # Expected output: 20
+print(century(2001))  # Expected output: 21
+
+#####################################################################################
+###############   bonus      ####################################################
+#####################################################################################
+
+# Szpanersko napisane "hello world!"
+
+def greet():
+    binDict = {
+        "0b1000001": "0110000101100011011001010111010001111001011011000110001101101000011011110110110001101001011011100110010101110011011101000110010101110010011000010111001101100101",
+        "0b1000010": "0110001001110101011000110110101101101101011010010110111001110011011101000110010101110010011001100111010101101100011011000110010101110010011001010110111001100101",
+        "0b1000011": "0110001101101000011011000110111101110010011011110110011001101100011101010110111101110010011011110110110101100101011101000110100001100001011011100110010101110011",
+        "0b1000100": "0110010001100101011010010110111001110011011101000110100101110100011101010111010001101001011011110110111001100001011011000110100101111010011010010110111001100111",
+        "0b1000101": "0110011001101111011100100110010101110100011010000110111101110101011001110110100001110100011001100111010101101100011011100110010101110011011100110110010101110011",
+        "0b1000110": "011001000110100101110011011101000110100101101110011001110111010101101001011100110110100000100000011000010110001001101001011011000110100101110100011010010110010101110011",
+        "0b1000111": "011100110111010001110010011000010110100101100111011010000111010001100110011011110111001001110111011000010111001001100100011011100110010101110011011100110110010101110011",
+        "0b1001000": "011011010111010101101100011101000110100101100100011010010110110101100101011011100111001101101001011011110110111001100001011011000110100101110100011010010110010101110011",
+        "0b1001001": "011010010110110101101101011101010110111001101111011001010110110001100101011000110111010001110010011011110111000001101000011011110111001001100101011101000110100101100011",
+        "0b1001010": "011100000110100001101111011101000110111101110000011010000110111101110011011100000110100001101111011100100111100101101100011000010111010001101001011011110110111001110011",
+        "0b1001011": "011100000110100001101111011100110111000001101000011011110110011101101100011110010110001101100101011100100110000101101100011001000110010101101000011110010110010001100101",
+        "0b1001100": "01110000011100110111100101100011011010000110111101110100011010000110010101110010011000010111000001100101011101010111010001101001011000110110000101101100011011000111100100100001",
+    }
+    sR = ""
+    posRefL = [7, 9, 7, 14, 6, 11, 11, 12, 16, 14, 19, 21]
+    for i in range(65, 77):
+        sR += binDropper(binDict[binDropper(chr(i))], True)[posRefL[i - 65]]
+    return sR
+
+
+def binDropper(pL, ship=False):
+    if ship:
+        return int(pL, 2).to_bytes((int(pL, 2).bit_length() + 7) // 8, 'big').decode()
+    return bin(int.from_bytes(pL.encode(), 'big'))
+
+
+print(greet())
+
+#jak to działa
+
+# Funkcja greet:
+
+# binDict to słownik, gdzie klucze to binarne reprezentacje wartości ASCII ('0b1000001', '0b1000010', ..., '0b1001100'), a wartości to odpowiadające im ciągi binarne.
+
+# posRefL to lista liczb całkowitych reprezentujących pozycje, z których będą pobierane znaki z ciągów binarnych.
+
+# Funkcja iteruje po wartościach ASCII od 65 do 76 (odpowiadających literom 'A' do 'L') przy użyciu pętli for i in range(65, 77). W każdej iteracji konwertuje wartość ASCII na znak (chr(i)) i używa go jako klucza do uzyskania odpowiadającego ciągu binarnego z binDict.
+
+# Następnie funkcja przekazuje uzyskany ciąg binarny do funkcji binDropper, która wyciąga konkretny znak na podstawie określonych pozycji zdefiniowanych w posRefL. Wyciągnięte znaki są konkatenowane, tworząc ostateczny ciąg sR.
+
+# Ostateczny ciąg sR jest zwracany.
+
+# Funkcja binDropper:
+
+# Jeśli ship jest True, konwertuje ciąg binarny pL na liczbę całkowitą, następnie konwertuje tę liczbę na bajty, a ostatecznie dekoduje bajty na ciąg znaków. W zasadzie dokonuje konwersji danych binarnych na ciąg znaków.
+
+# Jeśli ship jest False, konwertuje wejściowy ciąg znaków pL na bajty, a następnie konwertuje te bajty na reprezentację binarną.
+
+# Instrukcja print(greet()) wywołuje funkcję greet i drukuje uzyskany ciąg znaków.
+
+# Ogólnie rzecz biorąc, kod wydaje się być kreatywnym sposobem kodowania wiadomości przy użyciu ciągów binarnych i wyciągania konkretnych znaków na podstawie wcześniej zdefiniowanych pozycji. Funkcja binDropper służy do obsługi konwersji z binarnego na tekstowy i odwrotnie. Funkcja greet wykorzystuje te konwersje do stworzenia konkretnej zakodowanej wiadomości na podstawie wartości ASCII i pozycji.
