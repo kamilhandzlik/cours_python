0f518cebdfa1b42c6b5fd41399f92f5c18785521
Kamil Handzlik <kamol0209@gmail.com>
2024-02-29 20:05:00 +0100

    added start of my solution to exercise 6 from bootcamp and added my daily practice

--- code_wars.py
+++ code_wars.py
@@ -3673,14 +3673,32 @@ Examples:(Input1, Input2 --> Output (explanation)))
 
 
 def add_binary(a, b):
-    result = bin(a + b)[2:]
-    return result
+    return bin(a + b)[2:]
 
 
-print(add_binary(2, 2))
 #####################################################################################
 ###############   ZADANIE 100     ###################################################
 #####################################################################################
+"""You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+Examples:(Input --> Output)
+
+121 --> 144
+625 --> 676
+114 --> -1 since 114 is not a perfect square"""
+
+from math import sqrt
+
+
+def find_next_square(sq):
+    if sqrt(sq).is_integer():
+        return int((sqrt(sq) + 1) ** 2)
+    else:
+        return -1
 
 
 #####################################################################################
