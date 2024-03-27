9ad8087386dc78c988d97c0dc9609015fa0fd07c
Kamil Handzlik <kamol0209@gmail.com>
2024-03-27 20:07:45 +0100

    my daily practice

--- code_wars_2.py
+++ code_wars_2.py
@@ -304,6 +304,36 @@ def switch_it_up(number):
 #####################################################################################
 ###############   ZADANIE 109     ###################################################
 #####################################################################################
+"""Your task is to write a function which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step.
+
+If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)"""
+
+
+# Rozwiązanie 1
+def sequence_sum(begin_number, end_number, step):
+    if begin_number > end_number:
+        return 0
+    else:
+        return sum(range(begin_number, end_number + 1, step))
+
+
+# Rozwiązanie 2
+def sequence_sum(begin_number, end_number, step):
+    return (
+        0
+        if begin_number > end_number
+        else sum(range(begin_number, end_number + 1, step))
+    )
+
 
 #####################################################################################
 ###############   ZADANIE 110     ###################################################
